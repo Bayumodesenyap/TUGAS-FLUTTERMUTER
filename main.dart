ef22c0f80e81e437ae090c3fc25a8fc8ba97c603
Bayumodesenyap <bayuuye890@gmail.com>
2024-10-22 12:26:52 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App', //diberi judul namer app
         theme: ThemeData( //data tema aplikasi diberi warna deepOrange
           useMaterial3: true, //versi material UI yang dipakai versi 3
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
         ),
         home: MyHomePage(), //nama halaman "MyHomePage"
       ),
@@ -41,21 +41,50 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+     var pair = appState.current;  
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-           ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-               appState.getNext();  // ← This instead of print().
-      
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, 
+          children: [
+            Text('A random idea:'),
+             BigCard(pair: pair),
+          
+             ElevatedButton(
+              onPressed: () {
+                print('button pressed!');
+                 appState.getNext();  // ← This instead of print().
+        
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+     final theme = Theme.of(context);  
+      final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+       color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+         child: Text(pair.asLowerCase, style: style,semanticsLabel: "${pair.first} ${pair.second}"),
       ),
     );
   }
